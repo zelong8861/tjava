1e32c6815f26ad708b2cef9815a29acf1bfd711c
Zelong Li(IT) <zelong.li@cicc.com.cn>
2019-01-18 10:49:06 +0800

    lambda

--- src/main/java/c01.java
+++ src/main/java/c01.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
+
+@FunctionalInterface
+interface PrintMsg {
+
+    void printMsg();
+
+}
+
 
 public class c01 {
 
@@ -48,11 +59,51 @@ public class c01 {
         return true;
     }
 
-    private static void testMerge () {
+    private static void testMerge() {
+    }
+
+    private static void execute(PrintMsg printer) {
+        printer.printMsg();
+    }
+
+    private static void doCompute(lambdaFunc lambdaFunc) {
+        lambdaFunc.compute();
     }
 
     public static void main(String[] args) {
-       // testTrueFalse();
+        testTrueFalse();
         System.out.println(isValid("{[(}]}"));
+
+        /*
+         *
+         * lambda function
+         *
+         * */
+
+        execute(new PrintMsg() {
+            @Override
+            public void printMsg() {
+                System.out.println("hello world");
+            }
+        });
+
+        execute(() -> System.out.println("world hello"));
+
+        execute(() -> System.out.println("hello world !!!!!"));
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        list.forEach(n -> {
+            if (n % 2 == 0) System.out.println("偶数是：" + n);
+        });
+
+        List<String> listStr = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            listStr.add("bob" + String.valueOf(i));
+        }
+
+        listStr.forEach(n -> System.out.println(n.substring(0, 3)));
+
     }
 }
